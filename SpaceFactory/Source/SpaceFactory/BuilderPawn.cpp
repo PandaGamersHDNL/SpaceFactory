872cb28b0ac0ae40d5d9953f6d51f31e511ac406
panda jacobs <jokke.jacobs@gmail.com>
2021-01-31 01:17:33 +0100

    rotation builder

--- SpaceFactory/Source/SpaceFactory/BuilderPawn.cpp
+++ SpaceFactory/Source/SpaceFactory/BuilderPawn.cpp
@@ -9,8 +9,7 @@ ABuilderPawn::ABuilderPawn()
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	RootComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-
-	//AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform, TEXT("Mesh"));
+	SetActorRotation(FRotator(0.0f, -30.0f, 0.0f));
 }
 
 // Called when the game starts or when spawned
