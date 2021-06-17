1ce1710d9cb0dfc94a67264d97c87fd0e7df27d0
panda jacobs <jokke.jacobs@gmail.com>
2021-06-17 04:36:41 +0200

    add sell machine building. cpp bp and buy ui

---
+++ SpaceFactory/Source/SpaceFactory/SellMachine.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProcessMachine.h"
+#include "SellMachine.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEFACTORY_API ASellMachine : public AProcessMachine
+{
+	GENERATED_BODY()
+	
+};
